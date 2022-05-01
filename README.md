4544f6745c2599870e0a85c28d0814b9cb24f642
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-30 22:01:55 -0400

    Initial mod setup

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Quilt Mod Template
+# Charms & Hexes
 
-An unofficial mod-template for Quilt. Just to make it easier for my use when creating a new mod.
-
-## Credit
-
-This template on the QuiltMC GitHub is licensed under the [Creative Common Zero v1.0 license](./LICENSE.md).
-
-This template is based on the [quilt-example-mod](https://github.com/QuiltMC/quilt-example-mod) by [QuiltMC](https://github.com/QuiltMC).
+A mod for Minecraft that adds charms and hexes to Minecraft.
--- src/main/java/io/github/thepoultryman/charmsnhexes/CharmsAndHexes.java
+++ src/main/java/io/github/thepoultryman/charmsnhexes/CharmsAndHexes.java
@@ -1,11 +1,11 @@
-package io.github.thepoultryman.modid;
+package io.github.thepoultryman.charmsnhexes;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModClass implements ModInitializer {
+public class CharmsAndHexes implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
