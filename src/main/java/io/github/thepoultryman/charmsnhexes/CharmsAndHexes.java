84ebcc63d17f3e1b62a201c1ed3942281a533623
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-01 16:42:57 -0400

    Set up main class

--- src/main/java/io/github/thepoultryman/charmsnhexes/CharmsAndHexes.java
+++ src/main/java/io/github/thepoultryman/charmsnhexes/CharmsAndHexes.java
@@ -6,13 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CharmsAndHexes implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+    public static final String MOD_ID = "charmsnhexes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		LOGGER.info("Quilting Charms and Hexes");
 	}
 }
