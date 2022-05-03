13c4dbe54e6818a9d7ac8cff4457ac762efe1177
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-03 13:34:26 -0400

    Hardcode values for Bloody Damage

--- src/main/java/io/github/thepoultryman/charmsnhexes/hex/hexes/BloodyDamage.java
+++ src/main/java/io/github/thepoultryman/charmsnhexes/hex/hexes/BloodyDamage.java
@@ -15,12 +15,22 @@ public class BloodyDamage extends Hex {
 
     @Override
     public float getAttackDamage(int level, EntityGroup group) {
-        return Math.round(Math.pow(0.65f, -level));
+        return switch (level) {
+            case 1 -> 1f;
+            case 2 -> 1.5f;
+            case 3 -> 2.5f;
+            default -> (2.5f + (level * 0.1f));
+        };
     }
 
     @Override
     public void onTargetDamaged(LivingEntity user, Entity target, int level) {
-        user.damage(DamageSource.MAGIC, (float) Math.floor((Math.pow(0.55f, level) + 1f)));
+        switch (level) {
+            case 1 -> user.damage(DamageSource.MAGIC, 2f);
+            case 2 -> user.damage(DamageSource.MAGIC, 1.5f);
+            case 3 -> user.damage(DamageSource.MAGIC, 1f);
+            default -> user.damage(DamageSource.MAGIC, (1f - (level * 0.1f)));
+        }
     }
 
     @Override
