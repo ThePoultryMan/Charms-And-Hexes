f513048fe5101d786628dfe09463cb143f813b05
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-02 21:55:28 -0400

    this is severely cursed

--- src/main/java/io/github/thepoultryman/charmsnhexes/UniversalUtil.java
+++ src/main/java/io/github/thepoultryman/charmsnhexes/UniversalUtil.java
@@ -1,5 +1,8 @@
 package io.github.thepoultryman.charmsnhexes;
 
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+
 public class UniversalUtil {
     public static String getCharmTranslationKey(String name) {
         return "charm.name." + name;
@@ -7,5 +10,15 @@ public class UniversalUtil {
 
     public static String getHexTranslationKey(String name) {
         return "hex.name." + name;
+    }
+
+    public static boolean isCharm(ItemStack itemStack, String charmName) {
+        return itemStack.getItem() instanceof EnchantedBookItem
+            && EnchantedBookItem.getEnchantmentNbt(itemStack).get(0).asString().contains(charmName);
+    }
+
+    public static boolean isHex(ItemStack itemStack, String hexName) {
+        return itemStack.getItem() instanceof EnchantedBookItem
+            && EnchantedBookItem.getEnchantmentNbt(itemStack).get(0).asString().contains(hexName);
     }
 }
--- src/main/java/io/github/thepoultryman/charmsnhexes/mixin/EnchantmentBookInGameHudMixin.java
+++ src/main/java/io/github/thepoultryman/charmsnhexes/mixin/EnchantmentBookInGameHudMixin.java
@@ -3,21 +3,34 @@ package io.github.thepoultryman.charmsnhexes.mixin;
 import io.github.thepoultryman.charmsnhexes.UniversalUtil;
 import io.github.thepoultryman.charmsnhexes.registry.CharmRegistry;
 import io.github.thepoultryman.charmsnhexes.registry.HexRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class EnchantmentBookInGameHudMixin {
     @Shadow
     private ItemStack currentStack;
+
+    @Shadow
+    private int heldItemTooltipFade;
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    private String oldName;
 
     @ModifyVariable(at = @At("STORE"), method = "renderHeldItemTooltip", ordinal = 0)
     public MutableText charmsnhexes$changeEnchantmentBookColor(MutableText mutableText) {
@@ -38,5 +51,52 @@ public class EnchantmentBookInGameHudMixin {
         }
 
         return mutableText;
+    }
+
+    @Inject(at = @At("HEAD"), method = "tickPausable")
+    private void charmsnhexes$setOldName(CallbackInfo ci) {
+        boolean breakLoop = false;
+        if (this.client.player != null) {
+            for (String charmName : CharmRegistry.CHARMS) {
+                if (UniversalUtil.isCharm(currentStack, charmName)) {
+                    this.oldName = UniversalUtil.getCharmTranslationKey(charmName);
+                    breakLoop = true;
+                    break;
+                }
+            }
+
+            if (!breakLoop)
+                for (String hexName : HexRegistry.HEXES) {
+                    if (UniversalUtil.isHex(currentStack, hexName)) {
+                        this.oldName = UniversalUtil.getHexTranslationKey(hexName);
+                        break;
+                    }
+                }
+        }
+    }
+
+    @Inject(at = @At("TAIL"), method = "tickPausable")
+    private void charmsnhexes$itemNameFadeFix(CallbackInfo ci) {
+        if (this.client.player != null) {
+            ItemStack itemStack = this.client.player.getInventory().getMainHandStack();
+            boolean breakLoop = false;
+            if (oldName != null) {
+                for (String charmName : CharmRegistry.CHARMS) {
+                    if (UniversalUtil.isCharm(currentStack, charmName) && !oldName.equals(UniversalUtil.getCharmTranslationKey(charmName))) {
+                        this.heldItemTooltipFade = 40;
+                        breakLoop = true;
+                        break;
+                    }
+                }
+
+                if (!breakLoop)
+                    for (String hexName : HexRegistry.HEXES) {
+                        if (UniversalUtil.isHex(currentStack, hexName) && !oldName.equals(UniversalUtil.getHexTranslationKey(hexName))) {
+                            this.heldItemTooltipFade = 40;
+                            break;
+                        }
+                    }
+            }
+        }
     }
 }
