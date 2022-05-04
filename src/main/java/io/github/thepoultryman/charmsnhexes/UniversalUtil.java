55e7a0fec3af99501acdca512b8971c67754db65
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-03 21:18:49 -0400

    Add Charm and Hex descriptions (tooltip)

--- src/main/java/io/github/thepoultryman/charmsnhexes/UniversalUtil.java
+++ src/main/java/io/github/thepoultryman/charmsnhexes/UniversalUtil.java
@@ -2,6 +2,7 @@ package io.github.thepoultryman.charmsnhexes;
 
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.TranslatableText;
 
 import java.util.Random;
 
@@ -10,6 +11,14 @@ public class UniversalUtil {
 
     public static String getCharmTranslationKey(String name) {
         return "charm.name." + name;
+    }
+
+    public static String getCharmDescription(String name) {
+        return new TranslatableText("charm.description." + name).getString();
+    }
+
+    public static String getHexDescription(String name) {
+        return new TranslatableText("hex.description." + name).getString();
     }
 
     public static String getHexTranslationKey(String name) {
---
+++ src/main/java/io/github/thepoultryman/charmsnhexes/mixin/EnchantedBookToolTipMixin.java
@@ -0,0 +1,51 @@
+package io.github.thepoultryman.charmsnhexes.mixin;
+
+import io.github.thepoultryman.charmsnhexes.CharmsAndHexes;
+import io.github.thepoultryman.charmsnhexes.UniversalUtil;
+import io.github.thepoultryman.charmsnhexes.registry.CharmRegistry;
+import io.github.thepoultryman.charmsnhexes.registry.HexRegistry;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(EnchantedBookItem.class)
+public abstract class EnchantedBookToolTipMixin {
+    @Inject(at = @At("TAIL"), method = "appendTooltip")
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
+        NbtList enchantments = EnchantedBookItem.getEnchantmentNbt(stack);
+        for (int i = 0; i < enchantments.size(); ++i) {
+            NbtCompound nbtCompound = enchantments.getCompound(i);
+            Registry.ENCHANTMENT.getOrEmpty(EnchantmentHelper.getIdFromNbt(nbtCompound)).ifPresent((e) -> {
+                String name = e.getTranslationKey();
+                name = name.substring(name.indexOf(".", name.indexOf(".") + 1) + 1);
+                if (CharmRegistry.CHARMS.contains(name)) {
+                    tooltip.add(new LiteralText("---").formatted(Formatting.DARK_GRAY));
+                    for (String descriptionLine : UniversalUtil.getCharmDescription(name).split("\\n")) {
+                        tooltip.add(new LiteralText(descriptionLine).formatted(Formatting.ITALIC, Formatting.GRAY));
+                    }
+                }
+                else if (HexRegistry.HEXES.contains(name)) {
+                    CharmsAndHexes.LOGGER.info("uhh");
+                    tooltip.add(new LiteralText("---").formatted(Formatting.DARK_GRAY));
+                    for (String descriptionLine : UniversalUtil.getHexDescription(name).split("\\n")) {
+                        tooltip.add(new LiteralText(descriptionLine).formatted(Formatting.ITALIC, Formatting.GRAY));
+                    }
+                }
+            });
+        }
+    }
+}
